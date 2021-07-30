189dcaa813788cb810f7d93735d2a390fbfd469d
CSX777 <2771332246@qq.com>
2021-07-30 16:08:39 +0800

    cxh修改了代码

--- src/com/csx/githubTest.java
+++ src/com/csx/githubTest.java
@@ -4,5 +4,6 @@ public class githubTest {
     public static void main(String[] args) {
         System.out.println("练习github");
         System.out.println("csx修改了代码");
+        System.out.println("cxh修改了代码");
     }
 }
