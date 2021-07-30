154f1367970be5add5a6050c478557a8891dcd86
CSX777 <2771332246@qq.com>
2021-07-30 16:38:00 +0800

    csx修改了代码

--- src/com/csx/githubTest.java
+++ src/com/csx/githubTest.java
@@ -6,5 +6,6 @@ public class githubTest {
         System.out.println("csx修改了代码");
         System.out.println("csx再次修改了代码");
         System.out.println("cxh修改了代码");
+        System.out.println("csx练习gitee");
     }
 }
